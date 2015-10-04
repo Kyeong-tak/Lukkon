786afbf67306eb32fe7352e2cfe149bff343cd6c
blue1028 <blue1028@unist.ac.kr>
2015-10-04 23:32:24 +0900

    change22

--- Lukkon/urls.py
+++ Lukkon/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'',include('photo.urls')),
-    # url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
     # url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
 )
 # Test comment
--- photo/views.py
+++ photo/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Photo
 
 def single_photo(request, photo_id):
     return HttpResponse('{0}번 사진입니다'.format(photo_id))
+
+def post_list(request):
+    posts = Photo.objects.all().order_by('published_date')
+    return render(request, 'photo/index.html', {'posts' : posts})
+
